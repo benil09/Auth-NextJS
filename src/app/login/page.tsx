7d08a2f828a522cad1fc82174cf8fdd31d773b9c
benil09 <benil8235@gmail.com>
2024-12-26 20:39:18 +0530

    adding src files

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage(){
+        return (
+            <div className="flex border p-5">
+                <h1 className="font-bold text-3xl">LOGIN PAGE</h1>
+
+            </div>
+        )
+
+}
